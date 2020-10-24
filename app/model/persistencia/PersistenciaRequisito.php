9eab26e6e241347bb2716ac0ff2ef0d5285612b4
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2020-10-23 21:42:29 -0300

    criando as persistencias

--- app/model/persistencia/PersistenciaRequisito.php
+++ app/model/persistencia/PersistenciaRequisito.php
@@ -1,9 +1,21 @@
 <?php
 
 namespace app\model\persistencia;
+use app\util\Conexao;
+use app\model\entidade\Requisito;
 
 class PersistenciaRequisito
 {
+    private $conexao;
+    private $requisito;
+
+    public function __construct(Conexao $conexao, Requisito $requisito)
+    {
+        $this->conexao = $conexao->conectar();
+        $this->requisito = $requisito;
+    }
+
+
     public function cadastrar()
     {
 
--- app/model/persistencia/PersistenciaSistema.php
+++ app/model/persistencia/PersistenciaSistema.php
@@ -1,9 +1,21 @@
 <?php
 
 namespace app\model\persistencia;
+use app\util\Conexao;
+use app\model\entidade\Sistema;
+
 
 class PersistenciaSistema
 {
+    private $conexao;
+    private $sistema;
+
+    public function __construct(Conexao $conexao, Sistema $sistema)
+    {
+        $this->conexao = $conexao->conectar();
+        $this->sistema = $sistema;
+    }
+
 
     public function cadastrar()
     {
